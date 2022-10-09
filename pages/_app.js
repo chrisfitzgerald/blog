261f126c7c5566bf20990d38fdbaa536d8b46fa2
Chris Fitzgerald <cfitzgerald8888@gmail.com>
2022-10-08 23:12:41 -0400

    add posthob dependencies

--- pages/_app.js
+++ pages/_app.js
@@ -11,9 +11,19 @@ import siteMetadata from '@/data/siteMetadata'
 import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
+import { usePostHog } from 'next-use-posthog'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
+
+function MyApp({ Component, pageProps }) {  
+  // NOTE: If set as an environment variable be sure to prefix with `NEXT_PUBLIC_`
+  // For more info see https://nextjs.org/docs/basic-features/environment-variables#exposing-environment-variables-to-the-browser
+  
+  usePostHog('phc_ghIstnxf9s65JRzSJibDo20tWmA4wnKqptjgRJKPfm3', { api_host: 'https://app.posthog.com' })
+
+  return <Component {...pageProps} />
+}
 
 export default function App({ Component, pageProps }) {
   return (
