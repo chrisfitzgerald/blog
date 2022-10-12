e95bc06fe358247c05f4fe98adb64a8bef4e591b
Chris Fitzgerald <cfitzgerald8888@gmail.com>
2022-10-11 22:20:46 -0400

    updates to project page

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,15 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'logscan',
+    description: `A simple gui and script to help scan for known messages in large text files. Written in Python.`,
+    imgSrc: '/static/images/logscan.png',
+    href: 'https://github.com/chrisfitzgerald/logscan',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'StringReaderIO',
+    description: `Command Line prompt written in C# to experiment with user input and string manipulation`,
+    imgSrc: '/static/images/stringreaderio.jpg',
+    href: 'https://github.com/chrisfitzgerald/StringReaderIO',
   },
 ]
 
