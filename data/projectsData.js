852fac0d3acb95bbcae1bd971b6664b12f29ced4
Chris Fitzgerald <cfitzgerald8888@gmail.com>
2022-10-11 23:02:24 -0400

    logscan imgsrc

--- data/projectsData.js
+++ data/projectsData.js
@@ -2,7 +2,7 @@ const projectsData = [
   {
     title: 'logscan',
     description: `A simple gui and script to help scan for known messages in large text files. Written in Python.`,
-    imgSrc: '/static/images/logscan.png',
+    imgSrc: '/static/images/logScan.png',
     href: 'https://github.com/chrisfitzgerald/logscan',
   },
   {
