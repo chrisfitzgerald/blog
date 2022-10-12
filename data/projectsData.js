b5c8cfcbf0f70e999300d04b4852a2ee032cba0a
Chris Fitzgerald <cfitzgerald8888@gmail.com>
2022-10-11 22:49:14 -0400

    fix imgsrc for logscan and stringreader

--- data/projectsData.js
+++ data/projectsData.js
@@ -2,13 +2,13 @@ const projectsData = [
   {
     title: 'logscan',
     description: `A simple gui and script to help scan for known messages in large text files. Written in Python.`,
-    imgSrc: '/static/images/logscan.png',
+    imgSrc: './static/images/logscan.png',
     href: 'https://github.com/chrisfitzgerald/logscan',
   },
   {
     title: 'StringReaderIO',
     description: `Command Line prompt written in C# to experiment with user input and string manipulation.`,
-    imgSrc: '/static/images/stringreaderio.jpg',
+    imgSrc: './static/images/stringreaderio.jpg',
     href: 'https://github.com/chrisfitzgerald/StringReaderIO',
   },
 ]
