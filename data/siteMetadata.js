840db4251777c13244e272de03c15849a34a50d9
Chris Fitzgerald <cfitzgerald8888@gmail.com>
2023-04-30 00:39:47 -0400

    fixing giscus config

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -40,10 +40,10 @@ const siteMetadata = {
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      repo: 'chrisfitzgerald/blog',
+      repositoryId: 'R_kgDOIHd6eA',
+      category: 'General',
+      categoryId: 'DIC_kwDOIHd6eM4CWJp0',
       mapping: 'pathname', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
